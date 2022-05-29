20ac4a3cbda1d5841a120b976b4433f9f92102c0
Doron Chen <cdoron@il.ibm.com>
2022-05-29 12:40:03 +0300

    add parameters to main read and parse yaml config file

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/GIT_USER_ID/GIT_REPO_ID
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -10,14 +10,49 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 
 	openapi "github.com/GIT_USER_ID/GIT_REPO_ID/go"
 )
 
 func main() {
+	port := flag.Int("port", 8080, "Listening port")
+	var configFile string
+	flag.StringVar(&configFile, "config", "/etc/conf/conf.yaml", "")
+	flag.Parse()
+
+	fmt.Println("port:", *port)
+	fmt.Println("config file:", configFile)
+
+	// TODO - add logging level
+
 	log.Printf("Server started")
+
+	yamlFile, err := ioutil.ReadFile(configFile)
+
+	if err != nil {
+		panic(err)
+	}
+
+	conf := make(map[interface{}]interface{})
+
+	err = yaml.Unmarshal(yamlFile, &conf)
+	if err != nil {
+		panic(err)
+	}
+
+	uuidStr := fmt.Sprint(conf["uuid"])
+	fmt.Printf("Value: %s\n", uuidStr)
+	fmt.Printf("Value: %s\n", conf["etcd_hostname"])
+	fmt.Printf("Value: %d\n", conf["etcd_port"])
+	fmt.Printf("Value: %s\n", conf["etcd_username"])
+	fmt.Printf("Value: %s\n", conf["etcd_password"])
 
 	DefaultApiService := openapi.NewDefaultApiService()
 	DefaultApiController := openapi.NewDefaultApiController(DefaultApiService)
